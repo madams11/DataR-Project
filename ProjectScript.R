9b2ac516b98b1592e71fea19821d51688187a52e
madams11 <54282519+madams11@users.noreply.github.com>
2019-10-10 20:35:15 -0500

    Add files via upload

--- ProjectScript.R
+++ ProjectScript.R
@@ -16,15 +16,24 @@ library(readr)
 # Read CSV Files
 df <- read_csv("PBType.csv")
 accident <- read_csv("accident.csv")
+new <- left_join(df,accident, by = "ST_CASE",copy=TRUE)
+write_csv(new, "bikeaccidents.csv") 
+
+str(new)
 
 # Specily Column Types
     # ST_CASE: Integer?
-    # PBAGE: Integer?
-    
+    # PBAGE: Factor
+    # STATE: Factor
+    # 
 #my_cols
 
+str(new)
+
 # Remove Pedestrian Data Columns
-
+new <- select(new, c(-VEH_NO,-PEDCTYPE, -PEDLOC, -PEDPOS, -PEDDIR, -MOTDIR, -MOTMAN, -PEDLEG, -PEDSNR, -PEDCGP, -STATE.y, -VE_TOTAL, -VE_FORMS, -PVH_INVL, -PEDS, -PERNOTMVIT, -PERMVIT, -PERSONS, -COUNTY, -CITY, -NHS, -RD_OWNER, -ROUTE, -MILEPT, -SP_JUR, -RELJCT1, -RELJCT2, -RAIL, -NOT_HOUR, -NOT_MIN, -ARR_HOUR, -ARR_MIN, -HOSP_HR, -HOSP_MN))      # No longer needed
 # Only include Bicycle Fatalities
+new <- subset(new, BIKECTYPE != 0)
+#Merge Columns to create date
 
-# Merge with Accident Data
+
